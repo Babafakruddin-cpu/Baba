a3ca62311d4accbcc0891b066f3642af02317a37
Babafakruddin-cpu <62366695+Babafakruddin-cpu@users.noreply.github.com>
2020-03-20 10:04:08 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+/* var jhon ={
+    fullName: 'jhonchandr',
+    mass: 50,
+    height: 1.5,
+    caljhonBmi: function()
+    {
+        this.jhonBmi= this.mass/(this.height*this.height);
+        return this.jhonBmi;
+    }
+
+};
+var mark ={
+    fullName: 'markchandra',
+    mass: 60,
+    height: 1.3,
+    calmarkBmi: function()
+    {
+        this.markBmi= this.mass/(this.height*this.height);
+        return this.markBmi;
+    }
+
+};
+jhon.caljhonBmi();
+mark.calmarkBmi();
+console.log(jhon,mark);
+
+if(jhon.jhonBmi>mark.markBmi){
+    console.log(jhon.fullName + "is having more BMI than markchandra" + jhon.jhonBmi);
+}
+else if(mark.markBmi>jhon.jhonBmi){
+    console.log(mark.fullName + "is having more BMI than jhonchandra" + mark.markBmi);
+}
+else{
+    console.log("both having same BMI");
+}
+*/
